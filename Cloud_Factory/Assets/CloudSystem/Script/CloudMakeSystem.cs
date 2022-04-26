1f5d9c8333fd806bf7822cae00789acc7b329cfc
Marey <kamin_s2@g.hongik.ac.kr>
2022-04-27 01:58:05 +0900

    [CloudSystem] 구름 인벤토리로 구름 생성 구현

--- Cloud_Factory/Assets/CloudSystem/Script/CloudMakeSystem.cs
+++ Cloud_Factory/Assets/CloudSystem/Script/CloudMakeSystem.cs
@@ -13,11 +13,16 @@ public class CloudMakeSystem : MonoBehaviour
     ////////////////////////////////////////////////////////
     ///////            Interface Value               ///////
     ////////////////////////////////////////////////////////
-    public CloudSystem.EventHandler event_Selected;  //인벤토리에서 재료선택시 이벤트 호출_ 함수 명 변경하고 시픔...
-    public CloudSystem.EventHandler event_UnSelected;  // 선택된 재료 취소 
-    public CloudSystem.EventHandler event_createBtn; //구름제작 버튼
+    public CloudSystem.EventHandler E_Selected;  //인벤토리에서 재료선택시 이벤트 호출_ 함수 명 변경하고 시픔...
+    public CloudSystem.EventHandler E_UnSelected;  // 선택된 재료 취소 
+    public CloudSystem.EventHandler E_createCloud; //구름제작 버튼
 
+
+    //구름인벤토리 리스트_임시
+    public List<GameObject> L_cloudsInven;//외부에서 5개로 지정해놓음
+    //임시
     public Sprite default_sprite;//private으로 바꿀 예정.
+    public Sprite cloud_sprite;//Data structure로 바꿀 예정.
 
     ////////////////////////////////////////////////////////
     ///////            private Value                 ///////
@@ -43,9 +48,9 @@ public class CloudMakeSystem : MonoBehaviour
     //count
     private int total;
 
-    private void selectMtrl(string name)
+    private void d_selectMtrl(string name)
     {
-        if (total > 5) return; //최대 5개까지 선택 가능
+        if (total >= 5) return; //최대 5개까지 선택 가능
 
         //
         Debug.Log(name + "가 선택되었습니다.");
@@ -67,17 +72,17 @@ public class CloudMakeSystem : MonoBehaviour
         //
     }
 
-    private void deselectMtrl(string name)
+    private void d_deselectMtrl(string name)
     {
         GameObject ERSD = UI_slct_mtrl.Find(item => name == item.name); //삭제된 빈칸 찾기.
         ERSD.GetComponent<SpriteRenderer>().sprite =default_sprite;
 
         
-        listReOrder(UI_slct_mtrl.FindIndex(item => ERSD == item));
+        listreOrder(UI_slct_mtrl.FindIndex(item => ERSD == item));
     }
 
 
-    private void listReOrder(int idx) //리스트UI 정렬
+    private void listreOrder(int idx) //리스트UI 정렬
     {
         if (total <= 0) return;
 
@@ -100,14 +105,14 @@ public class CloudMakeSystem : MonoBehaviour
         UI_slct_mtrl[total].GetComponent<SpriteRenderer>().sprite = default_sprite;
     }
 
-    private void Del_ReadCSV(string name)
+    private void d_readCSV(string name)
     {
         //구름 조합법 나오면 그때 스크립트 작성.
         Debug.Log("조합재료를 확인합니다.");
     }
-    private void Del_CreateCloud(string name)
+    private void d_createCloud(string name = null)
     {
-        if (total < 5)
+        if (total < 1)
         {
             Debug.Log("재료수가 부족합니다.");
             return;
@@ -143,6 +148,7 @@ public class CloudMakeSystem : MonoBehaviour
             UI_slct_mtrl[i].GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
         }
 
+
         //UI 초기화
         init_UI();
 
@@ -150,11 +156,36 @@ public class CloudMakeSystem : MonoBehaviour
         UI_btn_txt.text = "제작하기";
 
         Debug.Log("구름이 만들어졌습니다.");
+        m_saveCloud();
 
         yield break;
     }
 
+    //구름 생성 후 인벤토리에 저장
+    private void m_saveCloud()
+    {
+        //구름 인벤토리 리스트 가져와서
+        int cnt = 0;
 
+        //빈 인벤토리 추척해서 구름 넣기.
+        while(true)
+        {
+            if (cnt >= 5) break; //인벤토리 크기 초과하면 실행X
+            if (L_cloudsInven[cnt].GetComponent<Image>().sprite != default_sprite)
+            {
+                cnt++;
+                continue;
+            }
+            L_cloudsInven[cnt].GetComponent<Image>().sprite = cloud_sprite;
+            L_cloudsInven[cnt].GetComponent<Button>().onClick.AddListener(DEMOcreateCloud);
+            break;
+        }
+    }
+
+    public void DEMOcreateCloud()
+    {
+        Debug.Log("구름이 저장되었습니다.");
+    }
     private void init_UI()
     {
         for (int i = 0; i < total; i++)
@@ -164,6 +195,7 @@ public class CloudMakeSystem : MonoBehaviour
     }
 
 
+    //초기화 함수
     private void init()
     {
         T_mtrl = this.transform.Find("selectedIngredient").transform;
@@ -178,16 +210,24 @@ public class CloudMakeSystem : MonoBehaviour
         {
             UI_slct_mtrl.Add(T_mtrl.GetChild(i).gameObject);
         }
+        //구름 인벤토리 리스트도 레퍼런스로 가지고 오는게 좋을 것 같다.
+
 
         //event
-        event_Selected = selectMtrl;
-        
-
-        event_UnSelected = deselectMtrl;
-
-        event_createBtn = Del_ReadCSV;
-        event_createBtn += Del_CreateCloud;
+        m_setEvent();
     }
+
+    //eventmethod 로직 함수
+    private void m_setEvent()
+    {
+        E_Selected = d_selectMtrl;
+
+        E_UnSelected = d_deselectMtrl;
+
+        E_createCloud = d_readCSV;
+        E_createCloud += d_createCloud;
+    }
+
 
     ////////////////////////////////////////////////////////
     ///////                Pipeline                  ///////
--- Cloud_Factory/Assets/CloudSystem/Script/DemoFunc.cs
+++ Cloud_Factory/Assets/CloudSystem/Script/DemoFunc.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
 /*
  스크립트 작동 확인을 위한 이벤트 함수를 임시로 만든 스크립트입니다.
  */ 
@@ -10,28 +12,30 @@ public class DemoFunc : MonoBehaviour
     [SerializeField]
     CloudMakeSystem Cloudmakesystem;
 
-
-    public void clicked() //matarial in inventory selected
-    {   
-        Cloudmakesystem.event_Selected(EventSystem.current.currentSelectedGameObject.name);
-    }
-
-    public void unclicked() //matarial in cloudmaker deselected
-    {
-        Cloudmakesystem.event_UnSelected(EventSystem.current.currentSelectedGameObject.name);
-        Debug.Log("클릭");
-    }
-
-    public void makeCloudBtn() //btn of cloudmaker clicked
-    {
-        Cloudmakesystem.event_createBtn(EventSystem.current.currentSelectedGameObject.name);
-    }
-
-
     void Start()
     {
         //Load cloudSyetem with Finding obj tag
         Cloudmakesystem = GameObject.FindWithTag("CloudSystem").GetComponent<CloudMakeSystem>();
     }
+
+    
+    public void clicked() //matarial in inventory selected
+    {   
+        Cloudmakesystem.E_Selected(EventSystem.current.currentSelectedGameObject.name);
+    }
+
+    public void unclicked() //matarial in cloudmaker deselected
+    {
+        Cloudmakesystem.E_UnSelected(EventSystem.current.currentSelectedGameObject.name);
+        Debug.Log("클릭");
+    }
+
+    public void makeCloudBtn() //btn of cloudmaker clicked
+    {
+        Cloudmakesystem.E_createCloud(EventSystem.current.currentSelectedGameObject.name);
+    }
+
+
+   
 
 }

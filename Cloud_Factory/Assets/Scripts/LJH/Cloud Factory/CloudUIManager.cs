ad4541036f3c93ffb79ddaf29676b9aade64dd58
임재혁 <86766590+limjh1@users.noreply.github.com>
2022-08-09 19:25:30 +0900

    220809-2

--- Cloud_Factory/Assets/Scripts/LJH/Cloud Factory/CloudUIManager.cs
+++ Cloud_Factory/Assets/Scripts/LJH/Cloud Factory/CloudUIManager.cs
@@ -72,6 +72,7 @@ public class CloudUIManager : MonoBehaviour
         else
         {
             ToggleCheckBox(mGiveCloudCheckBox[(int)ECheckBox.Emotion]);
+            ToggleDropBox(mSortDropBox);
         }
     }
 

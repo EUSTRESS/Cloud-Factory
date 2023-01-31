f700b79db70fe84630f031a5f68dbd131a0468dc
Dong-0211 <k010211@naver.com>
2023-01-31 10:16:28 +0900

    전반적인 애니메이션 수정

--- Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Cloud_movement.cs
+++ Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Cloud_movement.cs
@@ -7,52 +7,60 @@ public class Cloud_movement : MonoBehaviour
     public int num = 0;
     //public float limintMin = -5.0f;
     //private bool Act = false;
-    public GameObject cloud;
-    public GameObject parts;
-    public GameObject Parts_fly;
+    public GameObject cloud_part;        // 
+    public GameObject parts;        // 프리팹을 인스턴스화하여 담을 게임오브젝트
+    public GameObject Parts_fly;    // part_fly 스크립트 담겨져있는 프리팹담을 게임오브젝트
 
     void Start()
     {
-        num = Random.Range(1, 5);
-        GameObject go = Instantiate(parts); 
+        num = Random.Range(1, 5); // 네가지위치 무작위생성하는 변수
+        //GameObject go = Instantiate(parts);
         switch (num)
         {
             case 1:
-                cloud.transform.localPosition = new Vector2(-0.5f, 0.23f);
-                go.transform.position = cloud.transform.position;
-                Parts_fly.GetComponent<Parts_fly>().change_speed_1_2();
+                cloud_part.transform.localPosition = new Vector2(-0.5f, 0.23f);         // 왼쪽위 
+                InvokeRepeating("fly", 2f, 2f);                                                   // 파츠가 날라가는 함수 반복하는것(함수가 2초후부터 실행되고 2초주기로 실행)
+                Parts_fly.GetComponent<Parts_fly>().change_speed_1_2();              // 
                 break;
 
             case 2:
-                cloud.transform.localPosition = new Vector2(0.57f, 0.31f);
-                go.transform.position = cloud.transform.position;
+                cloud_part.transform.localPosition = new Vector2(0.57f, 0.31f);
+                InvokeRepeating("fly", 2f, 2f);
                 Parts_fly.GetComponent<Parts_fly>().change_speed_1_2();
                 break;
 
             case 3:
-                cloud.transform.localPosition = new Vector2(-0.29f, -0.29f);
-                go.transform.position = cloud.transform.position;
+                cloud_part.transform.localPosition = new Vector2(-0.29f, -0.29f);
+                InvokeRepeating("fly", 2f, 2f);
                 Parts_fly.GetComponent<Parts_fly>().change_speed_3();
                 break;
 
             case 4:
-                cloud.transform.localPosition = new Vector2(0.34f, -0.1f);
-                go.transform.position = cloud.transform.position;
+                cloud_part.transform.localPosition = new Vector2(0.34f, -0.1f);
+                InvokeRepeating("fly", 2f, 2f);
                 Parts_fly.GetComponent<Parts_fly>().change_speed_4();
                 break;
+
 
         }
 
 
 
+
     }
+
+    void fly()
+    {
+        GameObject go = Instantiate(parts);
+        go.transform.position = cloud_part.transform.position;
+    }
+
     void Update()
     { 
         if (Input.GetKeyDown(KeyCode.S))
         {
             GameObject go = Instantiate(parts);
-            go.transform.position = cloud.transform.position;
-            //Act = true;
+            go.transform.position = cloud_part.transform.position;
         }
     }
 }

83677ce6cb402d9e9d13bca166ef6110fefc85a1
Dong-0211 <k010211@naver.com>
2023-06-02 23:32:22 +0900

    구름파츠이펙트 수정
    그래픽분들과 협의하에
    파츠나오는 위치고정, 나오는 속도, 나오는각도 스크립트에서 수정

--- Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Destroy_ob.cs
+++ Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Destroy_ob.cs
@@ -9,26 +9,22 @@ public class Destroy_ob : MonoBehaviour
     public GameObject part_prefab_2;
     public Vector2 limitMax = new Vector2(8.9f, 5.0f);
     public Vector2 limitMin = new Vector2(-8.9f, -5.0f);
-    public Vector2 Cloud_position = new Vector3(0f, 0f, 0f);
+    //public Vector2 Cloud_position = new Vector3(0f, 0f, 0f);
 
+    //private int pos_Num;
+    //private int pos_Num_2;
 
     void Start()
     {
-        
+        //Cloud_movement cloud_Movement = GameObject.Find("MoveCloud").GetComponent<Cloud_movement>();
+        //pos_Num = cloud_Movement.ran_num;
+        //pos_Num_2 = cloud_Movement.ran_num2;
     }
 
     void Update()
     {
-        Cloud_position = FindObjectOfType<CloudSpawner>().Cloud_ps;
+        //Cloud_position = FindObjectOfType<CloudSpawner>().Cloud_ps;
 
-        if (part_prefab.transform.position.y < Cloud_position.y - 0.5f)
-        {
-            Destroy(gameObject);
-        }
-        if (part_prefab_2.transform.position.y < Cloud_position.y - 0.5f)
-        {
-            Destroy(gameObject);
-        }
 
         if (part_prefab.transform.position.x < limitMin.x || part_prefab.transform.position.x > limitMax.x || part_prefab.transform.position.y < limitMin.y || part_prefab.transform.position.y > limitMax.y)
         {
--- Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Parts_fly2.cs
+++ Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Parts_fly2.cs
@@ -10,17 +10,25 @@ public class Parts_fly2 : MonoBehaviour
     private float fly_speed = 1.0f;
     private float fly_angle = 45.0f;
     private float fly_angle2 = 60.0f;
+    private float fly_angle3 = 30.0f;
     private float height;
 
     public float fly_gravity = 9.8f;
     private float flying_time = 0f;
 
+    public float destroy_time;
+
     private Vector2 pos2 = new Vector2(0, 0);
 
     public int pos_Num_2 = 0;
 
+    private Vector3 plus_scale;
+
     void Start()
     {
+        plus_scale = new Vector3(0.0001f, 0.0001f, 0f);
+
+        destroy_time = 0f;
         Cloud_movement cloud_Movement_2 = GameObject.Find("MoveCloud").GetComponent<Cloud_movement>();
         pos_Num_2 = cloud_Movement_2.ran_num2;
 
@@ -49,18 +57,34 @@ public class Parts_fly2 : MonoBehaviour
 
         if (pos_Num_2 == 1 || pos_Num_2 == 3)
         {
-            float x2 = fly_speed2 * Mathf.Cos(fly_angle2 * Mathf.Deg2Rad);
+            float x2 = fly_speed2 * Mathf.Cos(fly_angle3 * Mathf.Deg2Rad);
             float y2 = height - (0.5f * fly_gravity * flying_time * flying_time);
-            Vector2 pos2 = Rigidbody.position + new Vector2(x2, y2) * Time.fixedDeltaTime;
+            Vector2 moveVector = new Vector2(x2, y2) * Time.fixedDeltaTime;
+            Vector2 pos2 = Rigidbody.position + moveVector;
+            destroy_time += Time.deltaTime;
             Rigidbody.MovePosition(pos2);
+            this.gameObject.transform.localScale += plus_scale;
+            if (destroy_time > 0.5f)
+            {
+                Destroy(gameObject);
+                destroy_time = 0f;
+            }
         }
 
         if (pos_Num_2 == 2 || pos_Num_2 == 4)
         {
             float x1 = fly_speed * Mathf.Cos(fly_angle2 * Mathf.Deg2Rad);
             float y1 = height - (0.5f * fly_gravity * flying_time * flying_time);
-            Vector2 pos1 = Rigidbody.position + new Vector2(x1, y1) * Time.fixedDeltaTime;
+            Vector2 moveVector = new Vector2(x1, y1) * Time.fixedDeltaTime;
+            Vector2 pos1 = Rigidbody.position + moveVector;
+            destroy_time += Time.deltaTime;
             Rigidbody.MovePosition(pos1);
+            this.gameObject.transform.localScale += plus_scale;
+            if (destroy_time > 0.5f)
+            {
+                Destroy(gameObject);
+                destroy_time = 0f;
+            }
         }
     }
 }

698a3931dbf8a22f02e29a3771f625b2cbe28b40
Dong-0211 <k010211@naver.com>
2023-07-08 18:12:33 +0900

    구름보내기씬리소스들 업데이트
    cloud storage씬 리스소들 업데이트
    ingPop		O
    btAlign_give	O
    btAlignB_give	O
    alignSliderF_give	O
    alignSliderB_give	O
    btAlignTri1_give	O
    btAlignTri2_give	O
    checkBox_give	O
    checkTick_give	O
    alignCheck_give	O
    btAL		O
    btAR		O
    invCloud3	O
    invCloudLid	O
    invCloudBG	O
    btGive1,2,3	O
    sliderCloudBG	O
    sliderCloudSq	O

--- Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Change_image.cs
+++ Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Change_image.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Change_image : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Change_image : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
     public Sprite return_Sprite;
     public Sprite Change_Sprite; // 마우스를 갖다대었을 때 보여줄 스프라이트
+    public Sprite Click_change_Sprite;
 
     public GameObject basic_image_obj;
 
@@ -20,6 +21,11 @@ public class Change_image : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     public void OnPointerEnter(PointerEventData eventData)
     {
         originalSprite.sprite = Change_Sprite; 
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        originalSprite.sprite = Click_change_Sprite;
     }
 
     public void OnPointerExit(PointerEventData eventData)

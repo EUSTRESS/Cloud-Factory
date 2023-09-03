94d70321ef160c82d4144d2d839588aaee67d97c
Dong-0211 <k010211@naver.com>
2023-09-04 00:01:15 +0900

    09.02 ~ 03 작업
    CF-284 적용중

--- Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Make_PartEffect.cs
+++ Cloud_Factory/Assets/Scripts/LDG/Cloud_Effect/Scripts/Make_PartEffect.cs
@@ -4,98 +4,132 @@ using UnityEngine;
 
 public class Make_PartEffect : MonoBehaviour
 {
-    public GameObject Parts_FadeEffect;
-    private GameObject New_FadeEffect;
+    CloudSpawner cloudSpawner;
 
-    public GameObject Parts_TornadoEffect;
-    private GameObject New_TornadoEffect;
+    Transform[] PartBody = new Transform[3];
+    Transform[] Part = new Transform[3];
 
-    public GameObject Parts_BummerEffect;
-    private GameObject New_BummerEffect;
+    // (리스트인덱스값) : (파츠이펙트프리팹이름) - (감정)
+    // 0 : FadeEffectCloud - 기쁨,혐오,관심&기대,얼어붙음,혼란스러움 / 1 : TornadoEffectCloud - 불안 / 2 : BummerEffectCloud - 슬픔,자책,씁쓸함 / 3 : HeartBeatEffectCloud - 짜증,반발,낙천,애증
+    // 4 : BounceEffectCloud - 수용,놀람&혼란,사랑 / 5 : TwinkleEffectCloud - 순종,경외 / 6 : FogEffectCloud - 경멸 / 7 : DiagonalEffectCloud - 공격성
+    public GameObject[] Make_EffectCloudObjects = new GameObject[8];
+    private GameObject[] New_EffectCloudObjects = new GameObject[8];
 
-    public GameObject Parts_HeartBeatEffect;
-    private GameObject New_HeartBeatEffect;
+    private Transform EffectCloudObj;
 
-    public GameObject Parts_BounceEffect;
-    private GameObject New_BounceEffect;
+    private Vector3 EffectGenerate_Pos;
 
-    public GameObject Parts_TwinkleEffect;
-    private GameObject New_TwinkleEffect;
+    // 0 : 기쁨,혐오,관심&기대,얼어붙음,혼란스러움 / 1 : 불안 / 2 : 슬픔,자책,씁쓸함 / 3 : 짜증,반발,낙천,애증
+    // 4 : 수용,놀람&혼란,사랑 / 5 : 순종,경외 / 6 : 경멸 / 7 : 공격성
+    public bool[] Emotions = new bool[8];
 
-    public GameObject Parts_DiagonalEffect;
-    private GameObject New_DiagonalEffect;
+    private bool Effect_Start;
 
-    public GameObject Parts_FogEffect;
-    private GameObject New_FogEffect;
-
+    private float InvokeTime;
     private float destroy_time;
+
+    public bool IsUsing;
 
     void Start()
     {
-        //InvokeRepeating("Make_FadeEffect_Cloud", 0.0f, 5.0f);
-        //InvokeRepeating("Make_TornadoEffect_Cloud", 0.0f, 5.0f);
-        //InvokeRepeating("Make_BummerEffect_Cloud", 0.0f, 5.0f);
-        //InvokeRepeating("Make_HeartBeatEffect_Cloud", 0.0f, 5.0f);
-        //InvokeRepeating("Make_BounceEffect_Cloud", 0.0f, 5.0f);
-        //InvokeRepeating("Make_TwinkleEffect_Cloud", 0.0f, 5.0f);
-        //InvokeRepeating("Make_DiagonalEffect_Cloud", 0.0f, 5.0f);
-        InvokeRepeating("Make_FogEffect_Cloud", 0.0f, 5.0f);
+        cloudSpawner = GameObject.Find("CloudSpawner").GetComponent<CloudSpawner>();
+        destroy_time = 0f;
+        InvokeTime = 0f;
+        for(int i = 0; i < 8; i++)
+        {
+            Emotions[i] = false;
+        }
+        Emotions[0] = true;
+        Effect_Start = false;
+        IsUsing = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        
+        EffectCloudObj = cloudSpawner.newTempCloud.transform;
+        EffectGenerate_Pos = cloudSpawner.newTempCloud.transform.position;
         destroy_time += Time.deltaTime;
-        if (destroy_time >= 5.0f)
+        InvokeTime += Time.deltaTime;
+
+        if (Emotions[0])
         {
-            //Destroy(New_FadeEffect);
-            //Destroy(New_TornadoEffect);
-            //Destroy(New_BummerEffect);
-            //Destroy(New_HeartBeatEffect);
-            //Destroy(New_BounceEffect);
-            //Destroy(New_TwinkleEffect);
-            //Destroy(New_DiagonalEffect);
-            Destroy(New_FogEffect);
+            Make_FadeEffect_Cloud(EffectCloudObj, EffectGenerate_Pos, cloudSpawner);
+            Emotions[0] = false;
+            Effect_Start = true;
+        }
+        else if (Emotions[1])
+        {
+            Make_TornadoEffect_Cloud(EffectCloudObj, EffectGenerate_Pos, cloudSpawner);
+            Emotions[1] = false;
+            Effect_Start = true;
+        }
+        else
+        {
+            if (InvokeTime >= 2.0f && Effect_Start == true) 
+            {
+                Emotions[0] = true;
+                InvokeTime = 0f;
+                Effect_Start = false;
+            }
+        }
+
+        if (destroy_time >= 2.0f)
+        {
+            Destroy(New_EffectCloudObjects[0]);
             destroy_time = 0.0f;
+        }
+        if (IsUsing)
+        {
+            this.gameObject.GetComponent<Make_PartEffect>().enabled = false;
+            if(New_EffectCloudObjects[0] != null) { Destroy(New_EffectCloudObjects[0]); }
         }
     }
 
-    void Make_FadeEffect_Cloud()
+
+    void Make_FadeEffect_Cloud(Transform TempTransform, Vector3 updatePos, CloudSpawner tempSpawner)
     {
-        New_FadeEffect = Instantiate(Parts_FadeEffect);
+        New_EffectCloudObjects[0] = Instantiate(Make_EffectCloudObjects[0], TempTransform);
+        New_EffectCloudObjects[0].transform.position = updatePos;
+        StoragedCloudData CData = tempSpawner.CloudData;
+
+        for(int i = 0; i < 3; i++)
+        {
+            PartBody[i] = New_EffectCloudObjects[0].transform.GetChild(i).transform;
+            Part[i] = PartBody[i].transform.GetChild(0).transform;
+        }
+        for (int i = 0; i < CData.mVPartsList.Count; i++)
+        {
+            Part[0].GetComponent<SpriteRenderer>().sprite = CData.mVPartsList[i].mImage;
+            Part[1].GetComponent<SpriteRenderer>().sprite = CData.mVPartsList[i].mImage;
+            Part[2].GetComponent<SpriteRenderer>().sprite = CData.mVPartsList[i].mImage;
+        }
+        Part[0].transform.localScale = new Vector3(0.11f, 0.11f, 0.5f);
+        Part[1].transform.localScale = new Vector3(0.11f, 0.11f, 0.5f);
+        Part[2].transform.localScale = new Vector3(0.16f, 0.16f, 0.5f);
     }
 
-    void Make_TornadoEffect_Cloud()
+    void Make_TornadoEffect_Cloud(Transform TempTransform, Vector3 updatePos, CloudSpawner tempSpawner)
     {
-        New_TornadoEffect = Instantiate(Parts_TornadoEffect);
+        New_EffectCloudObjects[1] = Instantiate(Make_EffectCloudObjects[1], TempTransform);
+        New_EffectCloudObjects[1].transform.position = updatePos;
+        StoragedCloudData CData = tempSpawner.CloudData;
+
+        for (int i = 0; i < 3; i++)
+        {
+            PartBody[i] = New_EffectCloudObjects[1].transform.GetChild(i).transform;
+            Part[i] = PartBody[i].transform.GetChild(0).transform;
+        }
+        for (int i = 0; i < CData.mVPartsList.Count; i++)
+        {
+            Part[0].GetComponent<SpriteRenderer>().sprite = CData.mVPartsList[i].mImage;
+            Part[1].GetComponent<SpriteRenderer>().sprite = CData.mVPartsList[i].mImage;
+            Part[2].GetComponent<SpriteRenderer>().sprite = CData.mVPartsList[i].mImage;
+        }
+        Part[0].transform.localScale = new Vector3(0.11f, 0.11f, 0.5f);
+        Part[1].transform.localScale = new Vector3(0.11f, 0.11f, 0.5f);
+        Part[2].transform.localScale = new Vector3(0.16f, 0.16f, 0.5f);
     }
 
-    void Make_BummerEffect_Cloud()
-    {
-        New_BummerEffect = Instantiate(Parts_BummerEffect);
-    }
-
-    void Make_HeartBeatEffect_Cloud()
-    {
-        New_HeartBeatEffect = Instantiate(Parts_HeartBeatEffect);
-    }
-
-    void Make_BounceEffect_Cloud()
-    {
-        New_BounceEffect = Instantiate(Parts_BounceEffect);
-    }
-    void Make_TwinkleEffect_Cloud()
-    {
-        New_TwinkleEffect = Instantiate(Parts_TwinkleEffect);
-    }
-
-    void Make_DiagonalEffect_Cloud()
-    {
-        New_DiagonalEffect = Instantiate(Parts_DiagonalEffect);
-    }
-
-    void Make_FogEffect_Cloud()
-    {
-        New_FogEffect = Instantiate(Parts_FogEffect);
-    }
 }
